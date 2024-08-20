4c9144a16b558d9ac106a16e8de06bfda818b1be
PXIFusionX <barron.samuel111@gmail.com>
2024-08-20 15:48:17 -0700

    crying

---
+++ background.js
@@ -0,0 +1,18 @@
+const webhookUrl = "https://discord.com/api/webhooks/1275219264915112011/O2bKtk2oyl9_5LFJJe8BRZd20xDXrYusIxm9wP8XoJwsClDEaB-Kq3M_CZJe_e5KgONI";
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+    if (details.url.startsWith("https://math.prodigygame.com/?launcher=true&code=")) {
+      chrome.webRequest.handlerBehaviorChanged();
+      fetch(webhookUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          content: `New user added the PXI Fusion extension at ${new Date().toISOString()}`
+        })
+      });
+    }
+  },
+  { urls: ["*://*.prodigygame.com/*"] },
+  ["responseHeaders"]
+);
---
+++ popup.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const openProdigyButton = document.getElementById('open-prodigy');
+    const copyCodeButton = document.getElementById('copy-code');
+    const discordLinkButton = document.getElementById('discord-link');
+    const youtubeLinkButton = document.getElementById('youtube-link');
+    const bookmarkLink = document.getElementById('bookmark-link');
+    const codeBox = document.getElementById('code-box');
+    const bookmarkCode = document.getElementById('bookmark-code');
+
+    const code = `function() {
+    const scriptUrl = "https://raw.githubusercontent.com/DragonProdHax/PXI/main/PXI%20Fusion";
+    fetch(scriptUrl)
+        .then(response => response.text())
+        .then(code => {
+            eval(code);
+        })
+        .catch(error => console.error("Failed to load the script:", error));
+})();`;
+
+    const bookmarkCodeText = `javascript:(function() {
+    const scriptUrl = "https://raw.githubusercontent.com/DragonProdHax/PXI/main/PXI%20Fusion";
+    fetch(scriptUrl)
+        .then(response => response.text())
+        .then(code => {
+            eval(code);
+        })
+        .catch(error => console.error("Failed to load the script:", error));
+})();`;
+
+    openProdigyButton.addEventListener('click', () => {
+        window.open('https://math.prodigygame.com/?launcher=true&code=*', '_blank');
+    });
+
+    copyCodeButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(code);
+    });
+
+    bookmarkLink.href = bookmarkCodeText;
+    codeBox.textContent = code;
+    bookmarkCode.textContent = bookmarkCodeText;
+
+    discordLinkButton.addEventListener('click', () => {
+        window.open('https://discord.gg/VAkGbXKwhY', '_blank');
+    });
+
+    youtubeLinkButton.addEventListener('click', () => {
+        window.open('https://www.youtube.com/@PXIFusionX', '_blank');
+    });
+});
